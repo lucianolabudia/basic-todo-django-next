0da05ad3acb6b160dc280b071df7c6a96826f687
Luciano Labudia <labudia.luciano@gmail.com>
2024-04-16 21:44:38 -0300

    Creating a basic ToDo with django and next

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,21 @@
+
+import FormTask from './components/FormTask'
+import ListTask from './components/ListTask'
+
+export const dynamic = 'force-dynamic'
+
+const HomePage = () => {
+  return (
+    <div className='container mx-auto'>
+        <h1>Tasks App</h1>
+
+        <div className='flex gap-x-10'>
+            <FormTask />
+            <ListTask />
+        </div>
+
+    </div>
+  )
+}
+
+export default HomePage
